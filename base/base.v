ff39cfca62dd666ed6aa4e1c23c2d4d65ad112c7
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-09-21 23:35:43 +0200

    fix effect

--- base/base.v
+++ base/base.v
@@ -1,11 +1,13 @@
 module base
 
-import linklancien.capas { Mark_config, Rules, Spell }
+import linklancien.capas { Rules, Mark_config, Rules, Spell, Spell_interface }
+import os {input}
 
 // Note: here all marks are initialised in an anticiped order so mark like regen can work
 // A: Init function
-// B: Marks effects function
-// C: Spell function
+// B: Turn_base_rule
+// C: Marks effects function
+// D: Spell function
 
 const id_pv = 0
 const id_shield = 1
@@ -36,7 +38,7 @@ pub fn init_rule_base(nb_team int) Rules {
 	}, Mark_config{
 		name:        'POISON'
 		description: 'Reduce the pv mark as much as the poison mark quantity'
-		effect:      pv_effect
+		effect:      poison_effect
 	}, Mark_config{
 		name:        'TARGET'
 		description: 'Used to store a spell target'
@@ -47,11 +49,42 @@ pub fn init_rule_base(nb_team int) Rules {
 	return rule
 }
 
-// B: Marks effects function
+// B: Turn_base_rule
+pub struct Turn_based_rules {
+pub mut:
+	rule      Rules
+	team_turn int
+	team_nb   int
+}
+
+pub fn (mut turn_based_rule Turn_based_rules) game() {
+	for turn_based_rule.rule.team_permanent_list[turn_based_rule.team_turn].len > 0 {
+		turn_based_rule.turn()
+		turn_based_rule.team_turn = (turn_based_rule.team_turn + 1) % 2
+	}
+	println('TEAM ${(turn_based_rule.team_turn + 1) % 2} WIN')
+}
+
+fn (mut turn_based_rule Turn_based_rules) turn() {
+	id := turn_based_rule.rule.get_mark_id('TARGET')
+	other_team_id := (turn_based_rule.team_turn + 1) % 2
+	max_target_id := turn_based_rule.rule.team_permanent_list[other_team_id].len - 1
+
+	for mut spell in mut turn_based_rule.rule.team_permanent_list[turn_based_rule.team_turn] {
+		spell.marks[id] = input('Select a target for ${spell.name} (-1 to target none, max: ${max_target_id}) : ').int()
+		spell.cast_fn[0](mut spell, mut turn_based_rule)
+	}
+
+	turn_based_rule.rule.all_marks_do_effect(other_team_id)
+	turn_based_rule.rule.update_permanent()
+	println('END TURN')
+}
+// C: Marks effects function
 fn pv_effect(id int, mut spells_list []Spell) {
 	for mut spell in spells_list {
 		if spell.marks[id] == 0 {
 			spell.is_ended = true
+			println('ER $id')
 		}
 	}
 }
@@ -87,22 +120,16 @@ fn target_effect(id int, mut spells_list []Spell) {
 	}
 }
 
-// C: Spell function
+// D: Spell function
 
-pub struct Turn_rules {
-pub mut:
-	rule      Rules
-	team_turn int
-	team_nb   int
-}
 
-pub fn attack(damage int, mut self Spell, mut rule Spell_interface) {
-	if mut rule is Turn_rules {
-		if rule.team_nb == 2 {
-			target := self.marks[rule.get_mark_id('TARGET')]
+pub fn attack(damage int, mut self Spell, mut costom_rule Spell_interface) {
+	if mut costom_rule is Turn_based_rules {
+		if costom_rule.team_nb == 2 {
+			target := self.marks[costom_rule.rule.get_mark_id('TARGET')]
 			if target >= 0 {
-				other_team_id := (rule.team_turn + 1) % 2
-				inflict_damage(damage, mut rule.team_permanent_list[other_team_id][target])
+				other_team_id := (costom_rule.team_turn + 1) % 2
+				inflict_damage(damage, mut costom_rule.rule.team_permanent_list[other_team_id][target])
 			}
 		} else {
 			panic('Not implemented')
@@ -113,7 +140,7 @@ pub fn attack(damage int, mut self Spell, mut rule Spell_interface) {
 // 1: handle flat reduction
 // 2: handle shield
 // 3: handle pv
-pub fn inflict_damage(damage int, mut spell Spell) {
+fn inflict_damage(damage int, mut spell Spell) {
 	// 1:
 	mut dmg := damage - spell.marks[id_flat_reduce_dmg]
 	// 2:
--- examples/example_base.v
+++ examples/example_base.v
@@ -1,18 +1,21 @@
 module main
 
 import base
-import linklancien.capas
+import linklancien.capas {Spell, Spell_const, Spell_interface}
 
 struct App {
-	base.Turn_rules
+	base.Turn_based_rules
 }
 
 fn main() {
 	mut app := App{}
-	app.rule = base.init_rule_base(2)
+	app.team_nb = 2
+	app.rule = base.init_rule_base(app.team_nb)
 
 	// println(app)
-
+	basic_attack := fn (mut self Spell, mut rule Spell_interface){
+		base.attack(1, mut self, mut rule)
+	}
 	app.rule.add_spell(0, Spell_const{
 		name:             'Test spell team 0'
 		cast_fn:          [basic_attack]
@@ -29,4 +32,16 @@ fn main() {
 			'TARGET': -1
 		}
 	})
+	app.init()
+	app.game()
+
+	println(app)
 }
+
+fn (mut app App) init() {
+	for team in 0 .. 2 {
+		app.rule.draw(team, 1)
+		app.rule.play_ordered(team, 1)
+	}
+}
+

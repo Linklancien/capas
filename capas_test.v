b59cb0b30895c69b4eee2ba222db13b7b73a3c1e
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-09-28 16:06:48 +0200

    New test

--- capas_test.v
+++ capas_test.v
@@ -107,26 +107,29 @@ fn name_array(rule capas.Rules) []string {
 }
 
 fn test_draw() {
-	mut rule := capas.rule_create(1, capas.Deck_type.classic)
+	names := [['Test spell 1', 'Test spell 2'], ['Test spell 1', 'Test spell 1']]
+	for name in names {
+		mut rule := capas.rule_create(1, capas.Deck_type.classic)
 
-	rule.add_spell(0, capas.Spell_const{
-		name: 'Test spell 1'
-	}, capas.Spell_const{
-		name: 'Test spell 2'
-	})
+		rule.add_spell(0, capas.Spell_const{
+			name: name[0]
+		}, capas.Spell_const{
+			name: name[1]
+		})
 
-	assert rule.team.deck[0].len == 2, 'len != 2, ${rule}'
-	assert rule.team.hand[0].len == 0, 'len != 0, ${rule}'
+		assert rule.team.deck[0].len == 2, 'len != 2, ${name}'
+		assert rule.team.hand[0].len == 0, 'len != 0, ${name}'
 
-	rule.draw(0, 1)
+		rule.draw_rand(0, 1)
 
-	assert rule.team.deck[0].len == 1, 'len != 1, ${rule}'
-	assert rule.team.hand[0].len == 1, 'len != 1, ${rule}'
+		assert rule.team.deck[0].len == 1, 'len != 1, ${name}'
+		assert rule.team.hand[0].len == 1, 'len != 1, ${name}'
 
-	rule.draw(0, 1)
+		rule.draw_rand(0, 1)
 
-	assert rule.team.deck[0].len == 0, 'len != 1, ${rule}'
-	assert rule.team.hand[0].len == 2, 'len != 1, ${rule}'
+		assert rule.team.deck[0].len == 0, 'len != 0, ${name}'
+		assert rule.team.hand[0].len == 2, 'len != 2, ${name}'
+	}
 }
 
 fn test_draw_rand() {
@@ -152,5 +155,42 @@ fn test_draw_rand() {
 
 		assert rule.team.deck[0].len == 0, 'len != 0, ${name}'
 		assert rule.team.hand[0].len == 2, 'len != 2, ${name}'
+	}
+}
+
+fn test_play_ordered() {
+	decks_types := [capas.Deck_type.classic, capas.Deck_type.dead_array]
+	for deck_type in decks_types {
+		mut rule := capas.rule_create(1, deck_type)
+
+		rule.add_spell(0, capas.Spell_const{
+			name: 'Test spell'
+		}, capas.Spell_const{
+			name: 'Test spell'
+		})
+
+		rule.draw_rand(0, 1)
+		rule.play_ordered(0, 1)
+
+		assert rule.team.hand[0].len == 0, 'len != 1, ${deck_type}'
+		assert rule.team.permanent[0].len == 1, 'len != 1, ${deck_type}'
+
+		rule.draw_rand(0, 1)
+		rule.play_ordered(0, 1)
+
+		assert rule.team.hand[0].len == 0, 'len != 1, ${deck_type}'
+		assert rule.team.permanent[0].len == 2, 'len != 1, ${deck_type}'
+
+		rule.team.permanent[0][0].is_ended = true
+		rule.team.update_permanent()
+		
+		match deck_type{
+			.classic{
+				assert rule.team.permanent[0].len == 1, 'len != 1, ${deck_type}'
+			}
+			.dead_array{
+				assert rule.team.permanent[0].len == 2, 'len != 1, ${deck_type}'
+			}
+		}
 	}
 }
